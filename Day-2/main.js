3b06333b6b99db78141cf51df04b503559d70b09
fahimad24 <mdtaraquerahmanfahim@gmail.com>
2026-03-18 17:08:15 +0600

    if friend adde show warning alart

--- Day-2/main.js
+++ Day-2/main.js
@@ -1,6 +1,10 @@
 const addBtn = document.getElementById("add-btn");
 
 addBtn.addEventListener("click", function () {
+    if (addBtn.innerText === "Added") {
+        toastr.warning('Friend is already added!', 'Warning');
+        return;
+    }
     addBtn.style.backgroundColor = "gray";
     addBtn.style.color = "white";
     addBtn.innerText = "Added";
